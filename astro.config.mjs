d2eda23e9b904607233701dc8581202b65f904ac
AkiraKs <adrien.lauriere@etu.unilim.fr>
2024-11-21 08:54:42 +0100

    test2

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://adrien-portfolio.github.io',
+    site: 'https://AkiraKS.github.io',
     integrations: [mdx(), sitemap(), react(), tailwind()], 
 });
