e9010df666d1aa9837730517076089620a560314
AkiraKs <adrien.lauriere@etu.unilim.fr>
2024-11-21 11:26:14 +0100

    oui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
 import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
@@ -11,5 +13,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://AkiraKS.github.io',
-    integrations: [mdx(), sitemap(), react(), tailwind()], 
+    integrations: [mdx(), sitemap(), react(), tailwind(), react()], 
 });
