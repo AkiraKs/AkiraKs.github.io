d66490d5885d80fc5e8a0ad22299c10982234fed
AkiraKs <adrien.lauriere@etu.unilim.fr>
2024-12-02 09:49:55 +0100

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://AkiraKS.github.io',
-    integrations: [mdx(), sitemap(), react(), tailwind()], 
+  site: "https://AkiraKS.github.io",
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                Passion: ['Passion One'],
-            },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Passion: ["Passion One"],
+      },
     },
-    plugins: [
-        function({ addBase }) {
-            addBase({
-                '*': {
-                    'scrollbar-width': 'thin',
-                    'scrollbar-color': '#6c59ff #1a1a1a',
-                },
-                '::-webkit-scrollbar': {
-                    width: '12px',
-                },
-                '::-webkit-scrollbar-track': {
-                    background: '#1a1a1a',
-                },
-                '::-webkit-scrollbar-thumb': {
-                    background: '#6c59ff',
-                    borderRadius: '6px',
-                    border: '3px solid #1a1a1a',
-                },
-                '::-webkit-scrollbar-thumb:hover': {
-                    background: '#8475ff',
-                },
-            });
+  },
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        "*": {
+          "scrollbar-width": "thin",
+          "scrollbar-color": "#6c59ff #1a1a1a",
         },
-    ],
-}
+        "::-webkit-scrollbar": {
+          width: "12px",
+        },
+        "::-webkit-scrollbar-track": {
+          background: "#1a1a1a",
+        },
+        "::-webkit-scrollbar-thumb": {
+          background: "#6c59ff",
+          borderRadius: "6px",
+          border: "3px solid #1a1a1a",
+        },
+        "::-webkit-scrollbar-thumb:hover": {
+          background: "#8475ff",
+        },
+      });
+    },
+  ],
+};
